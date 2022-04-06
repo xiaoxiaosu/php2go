031c0648115df48112e45ab1e7decff6fa35990e
苏宇豪 <suyh@weipaitang.com>
2022-04-06 13:47:40 +0800

    添加中间件

--- PHP2GO-第六课（徒手写http框架）/core.go
+++ PHP2GO-第六课（徒手写http框架）/core.go
@@ -7,6 +7,8 @@ import (
 type Core struct {
 	CoreConfig
 	router map[string] func(w http.ResponseWriter, r *http.Request)
+	middlewares []func(w http.ResponseWriter, r *http.Request)
+	index int
 }
 
 type CoreConfig struct {
@@ -17,7 +19,13 @@ func NewCore(conf CoreConfig) *Core{
 	return &Core{
 		conf,
 		make(map[string] func(w http.ResponseWriter, r *http.Request)),
+		make([]func(w http.ResponseWriter, r *http.Request),0),
+		0,
 	}
+}
+
+func (c *Core) Use(handlers ...func (w http.ResponseWriter, r *http.Request)) {
+	c.middlewares = append(c.middlewares, handlers...)
 }
 
 func (c *Core) Get(router string, handler func(w http.ResponseWriter, r *http.Request)) {
@@ -31,6 +39,13 @@ func (c *Core) Post(router string, handler func(w http.ResponseWriter, r *http.R
 }
 
 func (c *Core) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	// 处理中间件
+	for c.index<len(c.middlewares) {
+		c.middlewares[c.index](w, r)
+		c.index++
+	}
+
 	uri := r.RequestURI
 	method := r.Method
 	path := method + "-" + uri
--- PHP2GO-第六课（徒手写http框架）/test/main.go
+++ PHP2GO-第六课（徒手写http框架）/test/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	frame "go-frame"
 	"net/http"
 )
@@ -10,6 +11,10 @@ func main() {
 		Addr: ":8082",
 	}
 	c := frame.NewCore(conf)
+
+	c.Use(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("middleware")
+	})
 
 	c.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello"))
